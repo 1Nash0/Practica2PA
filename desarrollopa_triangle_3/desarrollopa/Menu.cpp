18967fc6f91059c92c33f2d0a938b218c5f0bf43
Nas <n.elkhiyat.2023@alumnos.urjc.es>
2025-01-15 17:11:43 +0100

    Título del juego

--- desarrollopa_triangle_3/desarrollopa/Menu.cpp
+++ desarrollopa_triangle_3/desarrollopa/Menu.cpp
@@ -2,26 +2,26 @@
 
 
 void Menu::Init() {
-    // Configura la cámara
+
     if (!camara) {
-        camara = new Camera(); //Suponemos que Camera tiene un constructor por defecto
-        camara->SetPosition(Vector3D(0, 0, 0)); // Establece una posición inicial
-        AddGameObject(camara); // Registra la cámara como un GameObject
+        camara = new Camera(); 
+        camara->SetPosition(Vector3D(0, 0, 0)); 
+        AddGameObject(camara); 
     }
 
-    // Texto del título del menú
+    // Texto del título 
     Text* titleText = new Text();
-    titleText->SetPosition(Vector3D(8, 7, 0)); // Posición relativa
-    titleText->SetText("INICIO");
+    titleText->SetPosition(Vector3D(7, 7, 0)); 
+    titleText->SetText("EMPTY SPACE");
     titleText->SetColor(Color(8.0f, 8.0f, 2.0f, 1.0f));
     textos.push_back(titleText);
     AddGameObject(titleText);
 
     // Texto de instrucciones
     Text* instructionText = new Text();
-    instructionText->SetPosition(Vector3D(4, 3, 0)); // Posición relativa
+    instructionText->SetPosition(Vector3D(4, 3, 0)); 
     instructionText->SetText("Presiona la tecla 'P' para iniciar el juego");
-    instructionText->SetColor(Color(8.0f, 0.0f, 0.0f, 1.0f)); // Granate
+    instructionText->SetColor(Color(8.0f, 0.0f, 0.0f, 1.0f)); 
     textos.push_back(instructionText);
     AddGameObject(instructionText);
 }
